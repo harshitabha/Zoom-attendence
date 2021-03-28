1e20495f63c7ae31bc06f562c0b2c368607d9447
Harshita Bhardwaj <honeyblue2604@gmail.com>
2021-03-27 20:29:40 -0700

    Nav Bar Fixed

---
+++ public/script.js
@@ -0,0 +1,11 @@
+
+//openning and closing sign in page
+openSignIn = function() {
+    document.getElementById("login").style.display="block";
+}
+
+closeSignIn = function() {
+    document.getElementById("login").style.display="none";
+}
+
+//Come back and try to get modal window close if there's time
